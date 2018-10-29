802cc4da25e0b6d1534a5822b8daec5a8ff3bb91
Kamilekkk <44519749+Ghetide@users.noreply.github.com>
2018-10-29 14:11:49 +0100

    Update rozdzial 2 zad 8.py

--- rozdzial 2 zad 8.py
+++ rozdzial 2 zad 8.py
@@ -1,3 +1,6 @@
-pesel = int(input("Podaj pesel: "))
-    
-            
+pesel = input("Podaj pesel: ")
+print(pesel)
+if "pesel[9]%2 == 1":
+    print("plec : mezczyzna")
+print(pesel[0],pesel[1])
+print("wiek :")
